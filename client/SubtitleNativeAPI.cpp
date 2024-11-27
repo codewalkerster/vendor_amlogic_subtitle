726c433982444f84e03b1a4c3cde65014af2c310
zhi.wang <zhi.wang@amlogic.com>
2024-11-27 02:59:44 +0000

    Subtitle: subtitle create wrong type. [1/1]
    PD#SWPL-191291
    
    Problem:
    subtitle create wrong type
    
    Solution:
    set correct subtitle type
    
    Verify:
    local
    
    Change-Id: I7177dd5bc06bd20a60fd0fa58a8a0657c4803d90
    Signed-off-by: zhi wang <zhi.wang@amlogic.com>

--- client/SubtitleNativeAPI.cpp
+++ client/SubtitleNativeAPI.cpp
@@ -70,23 +70,23 @@ static AmlSubDataType __mapServerType2ApiType(int type) {
 static int __mapApiType2SubtitleType(int type) {
     SUBTITLE_LOGI("call>> %s [stype:%d]", __func__, type);
     switch (type) {
-    case TYPE_SUBTITLE_DVB:
-        return DTV_SUB_DVB;
-    case TYPE_SUBTITLE_DVB_TELETEXT:
-        return DTV_SUB_DVB_TELETEXT;
-    case TYPE_SUBTITLE_SCTE27:
-        return DTV_SUB_SCTE27;
-    case TYPE_SUBTITLE_CLOSED_CAPTION:
-        return DTV_SUB_CC;
-    case TYPE_SUBTITLE_ARIB_B24:
-        return DTV_SUB_ARIB24;
-    case TYPE_SUBTITLE_TTML:
-        return DTV_SUB_DVB_TTML;
-    case TYPE_SUBTITLE_SMPTE_TTML:
-        return DTV_SUB_SMPTE_TTML;
+        case TYPE_SUBTITLE_DVB:
+            return DTV_SUB_DVB;
+        case TYPE_SUBTITLE_DVB_TELETEXT:
+            return DTV_SUB_DVB_TELETEXT;
+        case TYPE_SUBTITLE_SCTE27:
+            return DTV_SUB_SCTE27;
+        case TYPE_SUBTITLE_CLOSED_CAPTION:
+            return DTV_SUB_CC;
+        case TYPE_SUBTITLE_ARIB_B24:
+            return DTV_SUB_ARIB24;
+        case TYPE_SUBTITLE_TTML:
+            return DTV_SUB_DVB_TTML;
+        case TYPE_SUBTITLE_SMPTE_TTML:
+            return DTV_SUB_SMPTE_TTML;
+        default:
+            return DTV_SUB_INVALID;
     }
-    //default:
-    return type;
 }
 
 
